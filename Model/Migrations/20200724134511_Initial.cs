234acca6c4fcdb127606557f4039d780a5aca9cd
codernr <zoltan.csore@protonmail.com>
2020-07-24 15:46:57 +0200

    Create initial migration

---
+++ Model/Migrations/20200724134511_Initial.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TimedHostedServiceExample.Model.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "JobDatas",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Delay = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_JobDatas", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "JobDatas");
+        }
+    }
+}
